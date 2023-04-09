97c3fca4efcb552f9019b611eab404ca5f52990f
eyesight-pgs <eyesight.pgs@gmail.com>
2023-04-09 17:34:12 +0530

    format numbers

---
+++ single-files/format-numbers.rs
@@ -0,0 +1,18 @@
+
+
+fn main() {
+    let pi: f32 = 3.14;
+
+    // even this works
+    // let formatted: String = format!("{:0>10.5}", pi);
+    // print!("formatted pi: {}", formatted);
+
+    print!("formatted pi: {:0>10.5}", pi);
+}
+
+
+
+
+
+
+
