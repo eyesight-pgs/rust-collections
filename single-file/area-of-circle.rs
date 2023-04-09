45a4cbbe5fc199a2eac8e4a0b39ed4858f29c7f6
eyesight-pgs <eyesight.pgs@gmail.com>
2023-04-09 15:17:48 +0530

    area of circle

---
+++ single-file/area-of-circle.rs
@@ -0,0 +1,18 @@
+
+
+fn get_area_of_circle(r: f32) -> f32 {
+    return 2.0 * 3.1415 * 3.1415 * r;
+}
+
+
+fn main() {
+    let area: f32 = get_area_of_circle(10.0);
+    print!("area of circle = {}", area);
+}
+
+
+
+
+
+
+
