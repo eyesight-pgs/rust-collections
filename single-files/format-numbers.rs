da9e06a976e7682645b5e15baf8c8c889e80932e
eyesight-pgs <eyesight.pgs@gmail.com>
2023-04-09 17:42:40 +0530

    adding comments

--- single-files/format-numbers.rs
+++ single-files/format-numbers.rs
@@ -8,6 +8,17 @@ fn main() {
     // print!("formatted pi: {}", formatted);
 
     print!("formatted pi: {:0>10.5}", pi);
+
+    // meaning of 10.5 is:
+    //                                 .--5--.          <---- this is `.5`
+    //                                |       |               5 digits after decimal point
+    //   3 . 1 4  --->    0 0 0 0 3 . 1 4 0 0 0
+    //                    |                   |
+    //                     `--------10-------`          <---- this is `10`
+    //                                                        total 10 digits
+    //                                                        decimal point excluded
+    //
+
 }
 
 
